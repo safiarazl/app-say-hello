d6199ffc34604099bc894634f93c937e4624277b
111202012715@mhs.dinus.ac.id <111202012715@mhs.dinus.ac.id>
2022-07-19 19:21:46 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import(
 	"fmt"
 	go_say_hello "github.com/safiarazl/go-say-hello"
+	"time"
 )
 
 func main()  {
 	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(time.Now())
+	fmt.Println(time.Since(time.Date(2022, time.July, 18, 0, 0, 0, 0, time.UTC)))
 }
